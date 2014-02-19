8f0b97d247ab895f2984ae121c38a09303cfc1fc
Mike MacCana <mike.maccana@gmail.com>
2014-02-19 13:33:00 +0000

    Open a new A Soft Murmur tab if there's none open Also remove hard tabs

--- js/background.js
+++ js/background.js
@@ -1,23 +1,28 @@
 // Event page (formerly known as background script)
 // Get F5 presses from other tabs, send them to asoftmurmur tab
 
-
 var log = console.log.bind(console)
 
 log('A Soft Murmur keyboard: Background app running');
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	log('Recieved play/pause request');
-	log(request, sender, sendResponse)
-	// Find A Soft Murmur
-	chrome.tabs.query({'url':'http://asoftmurmur.com/*'}, function(tabs) {
-		if ( tabs.length) {
-			log('Found an asoftmurmur tab');
-			chrome.tabs.sendMessage(tabs[0].id, 'yo start or stop the noise or whatever')
-		} else {
-			// TODO: Maybe open a new asoftmurmur tab?
-			log('No asoftmurmur tabs are running')
-		}
-		console.dir(tabs)
-	})
+  log('Recieved play/pause request');
+  log(request, sender, sendResponse)
+  // Find A Soft Murmur
+  chrome.tabs.query({'url':'http://asoftmurmur.com/*'}, function(tabs) {
+    if ( tabs.length) {
+      log('Found an asoftmurmur tab');
+      chrome.tabs.sendMessage(tabs[0].id, 'yo start or stop the noise or whatever')
+    } else {
+      // TODO: Maybe open a new asoftmurmur tab?
+      log('No asoftmurmur tabs are running')
+      chrome.tabs.create({
+        url: 'http://asoftmurmur.com',
+        active: true
+      }, function(tab){
+        log('Opened new a Soft Murmur tab');
+      })
+    }
+    console.dir(tabs)
+  })
 });
